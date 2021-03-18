b98c5c21a51e72d33734f180a5156f6b0bb5a6a6
KIlnourik <kilnur1988@rambler.ru>
2021-03-18 14:30:53 +0500

    Выполнил домашнее задание Помощь друга (часть 2)

--- js/form-validation.js
+++ js/form-validation.js
@@ -6,30 +6,32 @@ const HASHTAG_MAX_LENGTH = 20;
 
 userCommentInput.addEventListener('input', () => {
   const userCommentLength = userCommentInput.value.length;
-
   if (userCommentLength > COMMENT_MAX_LENGTH) {
     userCommentInput.setCustomValidity(`Максимальная длина комментария ${COMMENT_MAX_LENGTH} знаков`);
   } else {
     userCommentInput.setCustomValidity('');
   }
-
   userCommentInput.reportValidity();
 });
 
-const hashtagLengthValidate = (arr) => {
-  for (let i = 0; i > arr.length; i++) {
-    if (arr[i].length > HASHTAG_MAX_LENGTH) {
-      return false;
+const hashtagLengthValidate = (hashTags) => {
+  for (let i = 0; i < hashTags.length; i++) {
+    if (hashTags[i].length <= HASHTAG_MAX_LENGTH) {
+      return true;
     }
-  };
+  }
 }
 
-const hashtagLettersValidate = (arr) => {
-  const relularExpression = /^#\w{1,19}$/;
+const hashtagLettersValidate = (hashTags) => {
+  return hashTags.every((tag) => /^#\w{1,19}$/i.test(tag));
+}
 
-  for (let i = 0; i > arr.length; i++) {
-    return relularExpression.test(i);
-  };
+const hashtagFirstLetterValidate = (hashTags) => {
+  for (let i = 0; i < hashTags.length; i++) {
+    if (hashTags[i].startsWith('#')) {
+      return true;
+    }
+  }
 }
 
 hashtagsInput.addEventListener('input', () => {
@@ -40,12 +42,14 @@ hashtagsInput.addEventListener('input', () => {
   } else {
     if (hashtags.length > HASHTAGS_MAX_COUNT) {
       hashtagsInput.setCustomValidity(`Максимум ${HASHTAGS_MAX_COUNT} хэштегов`);
-    } else if (hashtagLengthValidate(hashtags)) {
+    } else if (!hashtagLengthValidate(hashtags)) {
       hashtagsInput.setCustomValidity(`Макс. длина хэштега ${HASHTAG_MAX_LENGTH} зн.`);
     } else if (hashtags.length !== new Set(hashtags).size) {
       hashtagsInput.setCustomValidity('Хэштеги должны быть уникальны');
-    } else if (hashtagLettersValidate(hashtags)) {
-      hashtagsInput.setCustomValidity('Хэштеги должны начинаться с "#" и содержать только буквы и цифры');
+    } else if (!hashtagFirstLetterValidate(hashtags)) {
+      hashtagsInput.setCustomValidity('Хэштеги должны начинаться с "#"');
+    } else if (!hashtagLettersValidate(hashtags)) {
+      hashtagsInput.setCustomValidity('Хэштеги не может состоять только из "#" должны содержать только буквы и цифры');
     } else {
       hashtagsInput.setCustomValidity('');
     }
@@ -53,3 +57,13 @@ hashtagsInput.addEventListener('input', () => {
 
   hashtagsInput.reportValidity();
 });
+
+const onInputEscKeydown = (evt) => {
+  if (evt.key === 'Esc' || evt.key === 'Escape') {
+    evt.preventDefault();
+    evt.stopPropagation();
+  }
+}
+
+hashtagsInput.addEventListener('keydown', onInputEscKeydown);
+userCommentInput.addEventListener('keydown', onInputEscKeydown);
